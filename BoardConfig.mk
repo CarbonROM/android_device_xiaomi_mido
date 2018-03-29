d47b74d6438ff7cfeef7c9a4adf58235349ac80b
Christian Oder <myself5@carbonrom.org>
2018-03-29 23:40:06 +0200

    mido: Add carbon.dependencies and move kernel to kernel/xiaomi/mido

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_CONFIG := mido_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
+TARGET_KERNEL_SOURCE := kernel/xiaomi/mido
 
 # Kernel Toolchain
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linaro-7.2/bin
