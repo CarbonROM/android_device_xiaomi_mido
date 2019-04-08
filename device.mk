cf826e7276b2e5e9a7007e145ce2d8943318d18b
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    rolex: Adding GSI keys
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
 $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
