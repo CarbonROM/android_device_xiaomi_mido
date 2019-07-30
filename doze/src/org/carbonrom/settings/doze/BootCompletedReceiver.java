ca849ae09a875bd5f02c8f63b9b9c17dd5d22089
Roker2 <dimaminko2000@gmail.com>
2019-07-30 23:56:38 +0000

    mido: doze: Adapt for Carbon
    Change-Id: I471c9ebec675767a412501c80c9af9d688dbb1e3

--- doze/src/org/carbonrom/settings/doze/BootCompletedReceiver.java
+++ doze/src/org/carbonrom/settings/doze/BootCompletedReceiver.java
@@ -15,14 +15,14 @@
  * limitations under the License.
  */
 
-package org.lineageos.settings.doze;
+package org.carbonrom.settings.doze;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import org.lineageos.settings.doze.DozeUtils;
+import org.carbonrom.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
