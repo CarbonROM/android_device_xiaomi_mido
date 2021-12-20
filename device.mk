f83b9445c233c2decea5adb3244605fb226e1fd2
Wang Han <416810799@qq.com>
2021-12-20 16:45:33 +0800

    mido: configs: Import gpfspath_oem_config.xml
    * Opened by libdrmfs.so. Add it to be safe better than sorry.
    
    Change-Id: I46f33fba4591750a3c932e15b2b80a3c071ee4dd

--- device.mk
+++ device.mk
@@ -191,6 +191,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service-lazy \
     android.hardware.drm@1.3-service.clearkey
 
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/configs/gpfspath_oem_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gpfspath_oem_config.xml
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_mido \
