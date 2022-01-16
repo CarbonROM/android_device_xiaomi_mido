41ab106a98b05d36fa956585dbb8f4f0a28712a9
Roker2 <dimaminko2000@gmail.com>
2022-01-16 23:52:30 +0200

    Revert "mido: parts: update & add audio amp. title & summary"
    This reverts commit bc0681f810d0f8039067d03e3b560346deaa2dbe.

--- parts/src/org/lineageos/settings/preferences/SecureSettingsStore.java
+++ parts/src/org/lineageos/settings/preferences/SecureSettingsStore.java
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2017 AICP
  * Copyright (C) 2018 CarbonROM
- * Copyright (C) 2017-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
